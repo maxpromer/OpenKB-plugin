ed6bd0095a67d29d8b548eff524b52fa94be045e
Max <max30012540@hotmail.com>
2019-10-21 18:02:30 +0700

    Update th.js

--- openkb/msg/th.js
+++ openkb/msg/th.js
@@ -1,18 +1,18 @@
-Blockly.Msg.KB_IPST_DIGITAL_READ_MESSAGE = "อ่านค่าดิจิทัลที่ช่อง %1";
-Blockly.Msg.KB_IPST_DIGITAL_READ_TOOLTIP = "อ่านค่าดิจิทัลจากช่องใด ๆ บน KB-IPST";
+Blockly.Msg.OPENKB_DIGITAL_READ_MESSAGE = "อ่านค่าดิจิทัลที่ช่อง %1";
+Blockly.Msg.OPENKB_DIGITAL_READ_TOOLTIP = "อ่านค่าดิจิทัลจากช่องใด ๆ บน KB-IPST";
 
-Blockly.Msg.KB_IPST_DIGITAL_WRITE_MESSAGE = "เขียนค่าดิจิทัลที่ช่อง %1 เป็น %2";
-Blockly.Msg.KB_IPST_DIGITAL_WRITE_TOOLTIP = "เขียนค่าดิจิทัลไปที่ช่องใด ๆ บน KB-IPST";
+Blockly.Msg.OPENKB_DIGITAL_WRITE_MESSAGE = "เขียนค่าดิจิทัลที่ช่อง %1 เป็น %2";
+Blockly.Msg.OPENKB_DIGITAL_WRITE_TOOLTIP = "เขียนค่าดิจิทัลไปที่ช่องใด ๆ บน KB-IPST";
 
-Blockly.Msg.KB_IPST_ANALOG_READ_MESSAGE = "อ่านค่าแอนาล็อกที่ช่อง %1";
-Blockly.Msg.KB_IPST_ANALOG_READ_TOOLTIP = "อ่านค่าแอนาล็อก 0 ถึง 1023 จากช่องใด ๆ บน KB-IPST";
+Blockly.Msg.OPENKB_ANALOG_READ_MESSAGE = "อ่านค่าแอนาล็อกที่ช่อง %1";
+Blockly.Msg.OPENKB_ANALOG_READ_TOOLTIP = "อ่านค่าแอนาล็อก 0 ถึง 1023 จากช่องใด ๆ บน KB-IPST";
 
-Blockly.Msg.KB_IPST_ANALOG_WRITE_MESSAGE = "เขียนค่าแอนาล็อกที่ช่อง %1 เป็น %2";
-Blockly.Msg.KB_IPST_ANALOG_WRITE_TOOLTIP = "เขียนค่าแอนาล็อก 0 ถึง 1023 ไปที่ช่องใด ๆ บน KB-IPST";
+Blockly.Msg.OPENKB_ANALOG_WRITE_MESSAGE = "เขียนค่าแอนาล็อกที่ช่อง %1 เป็น %2";
+Blockly.Msg.OPENKB_ANALOG_WRITE_TOOLTIP = "เขียนค่าแอนาล็อก 0 ถึง 1023 ไปที่ช่องใด ๆ บน KB-IPST";
 
-Blockly.Msg.KB_IPST_SERVO_MESSAGE = "ตั้งเซอร์โวช่อง %1 หมุนไปที่ %2 องศา";
-Blockly.Msg.KB_IPST_SERVO_TOOLTIP = "";
+Blockly.Msg.OPENKB_SERVO_MESSAGE = "ตั้งเซอร์โวช่อง %1 หมุนไปที่ %2 องศา";
+Blockly.Msg.OPENKB_SERVO_TOOLTIP = "";
 
-Blockly.Msg.KB_IPST_SERVO_CALIBRATE_MESSAGE = "ตั้งเซอร์โวช่อง %1 ปรับเวลาเป็น ( %2 , %3 )";
-Blockly.Msg.KB_IPST_SERVO_CALIBRATE_TOOLTIP = "";
+Blockly.Msg.OPENKB_SERVO_CALIBRATE_MESSAGE = "ตั้งเซอร์โวช่อง %1 ปรับเวลาเป็น ( %2 , %3 )";
+Blockly.Msg.OPENKB_SERVO_CALIBRATE_TOOLTIP = "";
 
